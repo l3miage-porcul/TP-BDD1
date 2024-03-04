0cfb18a91d603c94eb03eed87b779c109a0363a0
Lorenzo <lorenzovongola38@hotmail.fr>
2024-03-04 16:30:59 +0100

    update BikeEntity et enums, ajout cr.md

--- cr.md
+++ cr.md
@@ -1,1 +1,5 @@
+1- Une table du même nom est crée.
 
+2- Cela permet de renommer une colonne et éventuellement d'ajouter des contraintes sur cette dernière
+
+3- Nécessaire pour donner les contraintes si le code n'est pas suffisant
--- server/src/main/java/fr/uga/l3miage/tp1/exo1/models/BikeEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp1/exo1/models/BikeEntity.java
@@ -8,30 +8,23 @@ import java.time.LocalDate;
 
 
 @Entity
-@Table(name="bike")
 public class BikeEntity {
 
     @Id
     @Column(name = "immat",length = 8)
     private String immatriculation;
 
-    @Column(name = "capacity")
     private Integer capacity;
 
-    @Column(name = "cylinderNumber")
     @Enumerated(EnumType.STRING)
     private CylinderNumber cylinderNumber;
 
-    @Column(name = "automatic")
     private boolean automatic;
 
-    @Column(name = "shifter")
     private boolean shifter;
 
-    @Column(name = "shifterType")
     @Enumerated(EnumType.ORDINAL)
     private ShifterType shifterType;
 
-    @Column(name= "circulation_date")
     private LocalDate circulationDate;
 }
